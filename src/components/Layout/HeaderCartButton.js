f3387743f8da00f12c1760a76ca34696c74e20db
PrachiRameshGodase <prachigodase2@gmail.com>
2023-06-03 17:57:35 +0530

    Showing and Enabling the delete of Cart Items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,34 @@
-import { useContext } from "react";
+import { useContext,useEffect,useState } from "react";
 import CartContext from "../../store/cart-context"
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton=(props) =>{
+  const [btnIsHighLighted,setbtnIsHighLighted]=useState(false);
   const cartCtx=useContext(CartContext);
+  const {items}=cartCtx;
+  
   const numberOfCartItems=cartCtx.items.reduce((currNumber,item)=>{
     return currNumber+item.amount;
   },0);
+  const btnClasses=`${classes.button} ${btnIsHighLighted ? classes.bump:""}`
+
+  useEffect(()=>{
+    if(cartCtx.items.length===0){
+      return;
+    }
+    setbtnIsHighLighted(true);
+
+    const timer=setTimeout(()=>{
+      setbtnIsHighLighted(false);
+    },300);
+
+    return()=>{
+      clearTimeout(timer);
+    }
+
+  },[items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
         <span>Your Cart</span>
         <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
